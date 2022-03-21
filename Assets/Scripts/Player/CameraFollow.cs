bbdbe9a1e886a062885bb6ab235cb7edcf25cd89
GoastcraftHD <simon.ickler@gmail.com>
2022-03-21 14:27:29 +0100

    Improved Camera Follow Sript

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -6,6 +6,6 @@ public class CameraFollow : MonoBehaviour
 
     void Update()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, -10f), 1 * Time.deltaTime);
     }
 }
